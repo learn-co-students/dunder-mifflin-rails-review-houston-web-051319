b7a769e7703a8a9e0c049ede56ca1d6962472cb4
Stefan Scepanovic <scepa91@icloud.com>
2019-06-14 13:03:36 -0500

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :title, uniqueness: { case_sensitive: false }
+    validates :alias, uniqueness: { case_sensitive: false }
+   
 end
